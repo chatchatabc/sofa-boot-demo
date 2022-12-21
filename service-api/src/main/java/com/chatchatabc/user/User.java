111b5ef0ac46e406ad9c47a859d345999cc10dca
Libing Chen <libing.chen@gmail.com>
2022-12-21 14:57:03 +0800

    api: User POJO

---
+++ service-api/src/main/java/com/chatchatabc/user/User.java
@@ -0,0 +1,26 @@
+package com.chatchatabc.user;
+
+
+public class User implements java.io.Serializable {
+    private Long id;
+    private String nick;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
