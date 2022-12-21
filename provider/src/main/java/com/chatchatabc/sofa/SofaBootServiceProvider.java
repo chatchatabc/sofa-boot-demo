d502f7e2e1db190ded3ac6e3edf19c303f2a01ac
Libing Chen <libing.chen@gmail.com>
2022-12-21 15:26:09 +0800

    app: SofaBootServiceProvider

---
+++ provider/src/main/java/com/chatchatabc/sofa/SofaBootServiceProvider.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.sofa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootServiceProvider {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootServiceProvider.class, args);
+    }
+
+}
