430e414101fbc68bb294140ef331e5c992c3a626
Libing Chen <libing.chen@gmail.com>
2022-12-21 15:41:25 +0800

    app: SofaBootServiceConsumer

---
+++ consumer/src/main/java/com/chatchatabc/consumer/SofaBootServiceConsumer.java
@@ -0,0 +1,14 @@
+package com.chatchatabc.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ImportResource({"classpath*:META-INF/spring/sofa-services-reference.xml"})
+public class SofaBootServiceConsumer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootServiceConsumer.class, args);
+    }
+
+}
