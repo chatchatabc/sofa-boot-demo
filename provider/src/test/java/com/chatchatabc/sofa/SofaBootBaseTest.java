81e79afdca9af71cc52e4ab269f627b9f304f4ab
Libing Chen <libing.chen@gmail.com>
2022-12-21 15:25:57 +0800

    test: SofaBootBaseTest

---
+++ provider/src/test/java/com/chatchatabc/sofa/SofaBootBaseTest.java
@@ -0,0 +1,8 @@
+package com.chatchatabc.sofa;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public abstract class SofaBootBaseTest {
+
+}
