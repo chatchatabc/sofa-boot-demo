9cf7c28f9e5dfe52c916cdfee646319386ab4d73
Libing Chen <libing.chen@gmail.com>
2022-12-27 12:22:08 +0800

    feat: add UserServiceFacade with @Autowired

--- consumer/src/main/java/com/chatchatabc/consumer/PortalController.java
+++ consumer/src/main/java/com/chatchatabc/consumer/PortalController.java
@@ -4,13 +4,22 @@ import com.alipay.sofa.runtime.api.annotation.SofaReference;
 import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
 import com.chatchatabc.user.User;
 import com.chatchatabc.user.UserServiceFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PortalController {
+    /**
+     * bound by SOFA RPC: easy to use
+     */
     @SofaReference(binding = @SofaReferenceBinding(bindingType = "bolt"))
+    private UserServiceFacade userServiceFacade1;
+    /**
+     * injected from Spring XML: prefer for maintainability and flexibility
+     */
+    @Autowired
     private UserServiceFacade userServiceFacade;
 
     @GetMapping("/user/{id}")
