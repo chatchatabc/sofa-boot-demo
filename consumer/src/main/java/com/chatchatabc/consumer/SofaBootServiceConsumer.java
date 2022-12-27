64e892a4f4bb7c955cc8b54a923e47a25be963ed
Libing Chen <libing.chen@gmail.com>
2022-12-27 12:21:23 +0800

    feat: add ImportResource

--- consumer/src/main/java/com/chatchatabc/consumer/SofaBootServiceConsumer.java
+++ consumer/src/main/java/com/chatchatabc/consumer/SofaBootServiceConsumer.java
@@ -2,9 +2,10 @@ package com.chatchatabc.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource({"classpath*:META-INF/spring/sofa-services-reference.xml"})
+@ImportResource({"classpath*:META-INF/spring/sofa-services-reference.xml"})
 public class SofaBootServiceConsumer {
 
     public static void main(String[] args) {
